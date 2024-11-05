c9983e66dda6378d569b7c77163ba406c21138b8
uday214125g <guggilla.uday143@gmail.com>
2024-11-05 19:56:30 +0530

    Add files via upload

---
+++ uday1.py
@@ -0,0 +1,179 @@
+#name = "uday"
+#n = 5
+#print("my name",name,"age is",n)
+
+
+#n = int(input())
+#for i in range(1,11):
+    #ans = n*i
+    #print(str(n)+" * "+ str(i)+" = "+str(ans))
+
+#n = int(input())
+#i = 1
+#while(i<=10):
+    #print(n,"*",i,"=",n*i)
+    #i +=1
+    
+#for i in range(1,101):
+    #if(i%5==0):
+        #print("dog")
+    #else:
+        #print(i)
+        
+
+#for i in range(1,101):
+    #if(i%2==0):
+        #print("cat")
+    #else:
+        #print(i)
+    #if(i%5==0):
+        #print("dog")
+    #else:
+        #print(i)        
+        
+        
+#for i in range(1,101):
+    #if(i%2==0 and i%3==0):
+        #print(i)
+        
+#important for as:
+#for i in range(1,101):
+    #if(i%3==0 and i%5==0):
+     #   print("fizz","buzz")
+    #elif(i%3==0):
+    #    print("fizz")
+   # elif(i%5==0):
+  #      print("buzz")
+            
+ #   else:
+#       print(i)      
+
+#item is adding into the square brackets
+#items = []
+#items.append("oil")
+#print(items)
+
+
+#adding the words
+#l = ["oil","salt",12,13,True,False]
+#l.append("maggie")
+#print(l)
+
+
+#items = []
+#items.append("uday:22M61A0487")
+#items.append("Nari:22M61A04C7")
+#print(items)
+
+#crush = []
+#crush.append("anushka")
+#crush.append("samantha")
+#crush.append("keerthisuresh")
+#crush.insert(1,"uday")
+#print(crush)
+
+#using extend method and reomve values, index value
+#crush = []
+#crush.append("anushka")
+#crush.append("samantha")
+#crush.append("keerthisuresh")
+#crush.insert(1,"uday")
+#crush.extend(["jahnavi"])
+#crush.remove("samantha")
+#print(crush.index("uday"))
+#print(crush)
+
+#using pop method
+#name = []
+#name.append("uday")
+#name.append("nari")
+#name.append("sharadeep")
+#name.pop(2)
+#print(name)
+
+#inserting the values
+#name = []
+#name.append("uday")
+#name.append("nari")
+#name.append("sharadeep")
+#name.insert(1,"uday")
+#print(name)
+
+
+#how many numbers are there.
+#rollno =[0,87,97,89,75,96]
+#print(len(rollno))
+
+#biggest number
+#rollno =[0,87,97,89,75,96]
+#print(max(rollno))
+
+#minimum number
+#rollno =[0,87,97,89,75,96]
+#print(min(rollno))
+
+#adding these numbers.
+#rollno =[0,87,97,89,75,96]
+#print(sum(rollno))
+
+#rollno are dessending order to assending order.
+#rollno =[0,87,97,89,75,96]
+#rollno.sort()
+#print(rollno)
+
+#sorting the contacts
+#contacts = ["uday","sharadeep","nari","nagi"]
+#contacts.sort()
+#print(contacts)
+
+#numbers in reverse order.
+#rollno = [0,87,97,89,75,96,]
+#rollno.sort()
+#rollno.reverse()
+#print(rollno)
+
+
+#second greatest value
+#rollno = [10,20,40,60,80,100]
+#rollno.sort()
+#print(rollno[-3])
+
+#division by 2
+#rollno = [0,84,3,9,44,42]
+#n = len(rollno)/2
+#n = int(n)
+#print(rollno[n])
+
+#score = int(input())
+#if(score>=90):
+    #grade = "A"
+#elif (score>=80 and score<=89):
+    #grade = "B"
+#elif (score>=70 and score<=79):
+    #grade = "C"
+#elif(score>=60 and score<=69):
+    #grade ="D"
+#else:
+    #grade ="F"
+#print("grade is:",grade)
+
+#sum of n natural numbers
+#n = int(input("Enter a number:"))
+#sum = 0
+#i = 1
+#while(i<=n):
+    #sum += i
+    #i += 1
+    #print(sum)
+
+
+#n = int(input("Enter a number:"))
+#sum = n*(n+1)//2
+#print(sum)
+
+#factorial program
+#n= int(input("Enter a number:"))
+#fact = 1
+#for i in range(1,n+1):
+    #fact *= i
+    #print(fact)
